2b3a57eb865b06e271045e53462e588dc6111844
Filipe Soccol <filipe.soccol@gmail.com>
2024-02-09 12:59:48 -0300

    Add burst script.

---
+++ burst-request.js
@@ -0,0 +1,13 @@
+const numberOfRequests = Number(process.argv[2])
+const url = process.argv[3]
+
+for (let i = 0; i < numberOfRequests; i++) {
+    fetch(url, {
+        method: 'GET'
+    }).then((res) => {
+        const json = res.status
+        console.log(json)
+    }).catch((error) => {
+        console.log('ERROR:', error)
+    });
+}
